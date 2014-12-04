bad4a9a2a320fd49f9357692a361adf077d5f8b9
Tom Clegg <tom@curoverse.com>
2014-12-04 10:51:37 -0500

    Port wh* to arv-*.

--- stores_in_warehouse.rb
+++ stores_in_warehouse.rb
@@ -18,13 +18,13 @@ module Longupload::StoresInWarehouse
       raise "Oops, #{self.class} #{self.id} block #{block_index} not found in staging area."
     end
 
-    Open3.popen3('whput', todo_file) do |std_in, std_out, std_err, wait_thr|
-      block_locator = std_out.gets.strip
+    Open3.popen3('arv-put', '--raw', todo_file) do |std_in, std_out, std_err, wait_thr|
+      block_locator = std_out.gets.strip rescue nil
       if wait_thr.respond_to? :value
         # ruby 1.9.3 actual exit status
         exitvalue = wait_thr.value
       else
-        # ruby 1.8.7 guess exit status based on output of whput
+        # ruby 1.8.7 guess exit status based on output of arv-put
         exitvalue = (block_locator && block_locator.match(/^[\da-f]{32}\b/) ? 0 : -1)
       end
       if block_locator and !block_locator.empty? and exitvalue == 0
@@ -36,17 +36,23 @@ module Longupload::StoresInWarehouse
         end
         File.unlink todo_file
       else
-        logger.error "whput #{self.class} #{self.id} block #{block_index} exited #{exitvalue}: #{std_err.gets}"
-        raise "Warehouse write failed"
+        logger.error "longupload #{self.class} #{self.id} arv-put block #{block_index} exited #{exitvalue}: #{std_err.gets}"
+        raise "Block write failed"
       end
     end
   end
 
   def after_longupload_file
     app_name = Rails.application.class.to_s.split('::').first
-    Open3.popen3('whput',
-                 "--name=/#{app_name}/#{ROOT_URL}/#{self.class}/#{self.id}",
-                 "-"
+    Open3.popen3('python', '-c', <<EOS,
+import arvados
+import sys
+print (arvados.api('v1').
+       collections().
+       create(body={'manifest_text': sys.stdin.read(), 'name': sys.argv[-1]}).
+       execute()['portable_data_hash'])
+EOS
+                 "#{app_name}--#{ROOT_URL}--#{self.class}--#{self.id}"
                  ) do |std_in, std_out, std_err, wait_thr|
       std_in.puts ". #{self.warehouse_blocks.join ' '} 0:#{self.longupload_size}:#{self.longupload_file_name.gsub ' ', '_'}"
       std_in.close
@@ -55,7 +61,7 @@ module Longupload::StoresInWarehouse
         # ruby 1.9.3 actual exit status
         exitvalue = wait_thr.value
       else
-        # ruby 1.8.7 guess exit status based on output of whput
+        # ruby 1.8.7 guess exit status based on output
         exitvalue = (new_manifest_locator && new_manifest_locator.match(/^[\da-f]{32}\b/) ? 0 : -1)
       end
       if new_manifest_locator and !new_manifest_locator.empty? and exitvalue == 0
@@ -65,8 +71,8 @@ module Longupload::StoresInWarehouse
           save!
         end
       else
-        logger.error "whput #{self.class} #{self.id} manifest exited #{exitvalue}: #{std_err.gets}"
-        raise "Warehouse write failed"
+        logger.error "longupload #{self.class} #{self.id} collections.create exited #{exitvalue}: #{std_err.gets}"
+        raise "Collection write failed"
       end
     end
   end
